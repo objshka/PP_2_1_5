58c71d42d022cf724400f17a283a1ad6fd9a57d2
objshka <91363173+objshka@users.noreply.github.com>
2023-05-07 20:31:58 +0300

    PP_2_1_5

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    @Autowired
+
     private Needle7 needle;
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
+    }
+
     @Override
     public String toString() {
         return ", в яйце иголка " + needle.toString();
